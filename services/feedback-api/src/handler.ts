f1023151048eaf05c7cca0e561ade58bf695935f
Nirankar.Jaiswar <nirankarjaiswar@gmail.com>
2026-08-22 22:45:08 +1200

    feat: Added feedback api service skeleton

---
+++ services/feedback-api/src/handler.ts
@@ -0,0 +1,8 @@
+export async function handler() {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Kiwi ScamCheck Feedback API is running",
+        })
+    }
+}
